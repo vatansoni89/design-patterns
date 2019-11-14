8571652d67598ecbd83d7c7cee2387767724acef
Vatan Soni <vatansoni89@gmail.com>
2019-11-14 12:36:51 +0530

    Add project files.

---
+++ MyCar/Bootstrap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using WonderTools.VendorContract;
+
+namespace WonderTools.MyCar
+{
+    class Bootstrap
+    {
+        static void Main(string[] args)
+        {
+            var configuration = GetConfiguration();
+
+            var carType = configuration.GetSection("carType").Value;
+            Console.WriteLine("The car type is :"+ carType);
+
+            var mode = configuration.GetSection("mode").Value;
+            Console.WriteLine("The mode is :" + mode);
+            var car = BuildCar(carType, mode);
+
+            var remote = new Remote(car);
+            remote.HandleUserInput();
+        }
+
+        
+
+        private static Car BuildCar(string carType, string mode)
+        {
+            var simulator = new CarSpeedSimulator();
+            var speedometer = new Speedometer(simulator);
+
+            IAlarm alarm = AlarmFactory.CreateAlarm(carType);
+            alarm = new VerifyingAlarmProxy(alarm, mode);
+
+            var speedAlarm = new SpeedAlarm();
+
+            var seatBelt = new SeatBelt();
+
+            Car car = new Car(alarm, seatBelt, simulator, speedometer, speedAlarm);
+            return car;
+        }
+
+        private static IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+            return configuration;
+        }
+    }
+}
---
+++ MyCar/Speedometer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WonderTools.MyCar
+{
+    public interface ISpeedSource
+    {
+        void AddSpeedObserver(ISpeedObserver speedObserver);
+    }
+
+    public class Speedometer : ISpeedSource
+    {
+        List<ISpeedObserver> _speedObservers = new List<ISpeedObserver>();
+
+        public Speedometer(CarSpeedSimulator speedSimulator)
+        {
+            speedSimulator.SpeedChanged += OnSpeedChanged;
+        }
+
+        private void OnSpeedChanged(int speed)
+        {
+            Console.WriteLine("Current Speed :" + speed);
+            foreach (var speedObserver in _speedObservers)
+            {
+                speedObserver.OnSpeedChange(speed);
+            }
+
+        }
+
+        public void AddSpeedObserver(ISpeedObserver speedObserver)
+        {
+            _speedObservers.Add(speedObserver);
+        }
+    }
+}
